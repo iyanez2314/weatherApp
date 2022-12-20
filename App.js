1b5beae609c2068816883fed5ef68c5b89fffd44
Isaac Yanez <isaac231467@gmail.com>
2022-12-20 13:40:44 -0600

    added some inital files I also added some JSX for the weather screen

--- App.js
+++ App.js
@@ -1,20 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// This file will host all the apps screens
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
+import WeatherScreen from "./src/screens/WeatherScreen";
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const navigator = createStackNavigator(
+  {
+    WeatherScreen: WeatherScreen,
   },
-});
+  {
+    initialRouteName: "WeatherScreen",
+    defaultNavigationOptions: {
+      title: "Weather App",
+    },
+  }
+);
+
+export default createAppContainer(navigator);
